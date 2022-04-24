a7c360b0274b18075177cb5ac92d96bf788b267d
eiress <104050918+eiress@users.noreply.github.com>
2022-04-23 22:29:14 -0600

    update

--- scripts/main.js
+++ scripts/main.js
@@ -4,4 +4,14 @@ console.log(date);
 let year = date.getFullYear();
 console.log(year);
 
-document.querySelector('div').textContent = year;
+//selecting element in HTML and DOM 
+//document.querySelector('p').textContent = year;
+
+// document.querySelector("img").setAttribute('src');
+
+
+//all I need for homework week 1
+let currentdate = document.lastModified;
+document.querySelector('span').textContent = currentdate;
+
+console.log(currentdate);
