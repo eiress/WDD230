00a227658cf72a434a2ab0ed64348551117058ff
eiress <104050918+eiress@users.noreply.github.com>
2022-05-02 11:44:59 -0600

    Update

---
+++ Lesson 2/scripts/design.js
@@ -0,0 +1,10 @@
+let date = new Date();
+console.log(date);
+
+let year = date.getFullYear();
+console.log(year);
+
+let currentdate = document.lastModified;
+document.querySelector('span').textContent = currentdate;
+
+console.log(currentdate);
