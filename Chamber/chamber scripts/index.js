3874f3834ef291b136a72cf90607d1b76cd79fc3
eiress <104050918+eiress@users.noreply.github.com>
2022-05-21 12:39:50 -0600

    new files

--- Chamber/chamber scripts/index.js
+++ Chamber/chamber scripts/index.js
@@ -1,8 +1,15 @@
 const datefield = document.querySelector(".date");
+const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+
+// derive the current date using a date object
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 	now
 );
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+// long, medium, short options ... try them
 
 datefield.innerHTML = `<em>${fulldate}</em>`;
-div or StereoPannerNode
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
---
+++ DOM/bom.js
@@ -0,0 +1,24 @@
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const list = document.querySelector("list");
+
+button.addEventListener('click', () => {
+    const listItem = document.createElement('li');
+    const listBtn = document.createElement('button');
+    const myItem = input.value;
+    input.value = "";
+
+
+   listItem.textContent=myItem;
+   listBtn.textContent = "X";
+    listItem.appendChild(listBtn);
+
+    list.appendChild(listItem);
+
+
+    listBtn.addEventListener('click', () => {
+      list.removeChild(listItem);
+    });
+
+    input.focus();
+  });
