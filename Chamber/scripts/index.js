486ae0dda0c1961de7e751e5f097a098eabcb6af
eiress <104050918+eiress@users.noreply.github.com>
2022-05-25 13:20:51 -0600

    update code

---
+++ Chamber/scripts/index.js
@@ -0,0 +1,16 @@
+let usDate = document.querySelector(".date");
+let datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+
+// derive the current date using a date object
+let now = new Date();
+let fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+let fulldateUK = new Intl.DateTimeFormat("en-UK", {	dateStyle: "full" }).format(now);
+// long, medium, short options ... try them
+
+usDate.innerHTML = `<em>${fulldate}</em>`;
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+
+
+document.querySelector(
+	"#lastModified"
+).textContent = `Last Modification: ${document.lastModified}`;
