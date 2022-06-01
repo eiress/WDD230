ea60932579feba792209852de66c85340ec4c3cd
eiress <104050918+eiress@users.noreply.github.com>
2022-06-01 12:01:25 -0600

    updated code

--- Chamber/scripts/windchill.js
+++ Chamber/scripts/windchill.js
@@ -1,9 +1,11 @@
-let temp = document.querySelector('.temp').textContent;
-let wind = document.querySelector('.wind').textContent;
-let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(wind,0.16)) + (0.4275*temp*Math.pow(wind,0.16)));
+let temp = document.getElementById("temp");
+let windspeed = document.getElementById('windSpeed');
 
-if(temp <= 50 && wind >= 30){
- chill;	
+if(temp <= 50 && windspeed >= 30){
+    let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
+    console.log(chill)
+    document.getElementById("windChill").textContent = chill + ' °F';
 }else{
-    N/A 
+    document.getElementById("windChill").textContent = "N/A"
 }
+console.log(temp);
