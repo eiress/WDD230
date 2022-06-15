995aae1b548958640627b9ecebe70e592c24e35b
eiress <104050918+eiress@users.noreply.github.com>
2022-06-15 13:12:02 -0600

    update join

---
+++ Lesson9/scripts/prophets.js
@@ -0,0 +1,55 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject); 
+    const prophets = jsonObject['prophets'];
+    prophets.forEach(displayProphets);
+        
+    }); 
+  
+
+// async function getProphets(requestURL){
+//     const response = await fetch(requestURL);
+//     if (response.ok){
+//         const jsonObject = await response.json();
+//         console.table(jsonObject);
+//     }
+// };
+
+// getProphets(requestURL)
+
+function displayProphets(prophet){
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let img = document.createElement('img');
+    let pob = document.createElement('p');
+    let dob = document.createElement('p');
+
+    h2.textContent = prophet.name + ' '+ prophet.lastname;
+    dob.textContent = `${prophet.birthdate}`;
+    pob.textContent = `${prophet.birthplace}`;
+    img.setAttribute('src', prophet.imageurl);
+    img.setAttribute
+    card.appendChild(h2);
+    card.appendChild(dob);
+    card.appendChild(img);
+    card.appendChild(pob);
+    document.querySelector('.cards').appendChild(card);
+}
+
+function displayTable(prophet){
+    let list_row = document.createElement('tr');
+    let td_name = document.createElement('td');
+    td_name.textContent = prophets.name + ' ' + prophet.lastname;
+
+    let td_birthdate = document.createElement('td');
+    td_birthdate.textContent = prophet.birthdate;
+
+    list_row.appendChild(td_name);
+    list_row.appendChild(td_birthdate);
+    document.querySelector('table').appendChild(list_row);
+}
