0de061e7cd7d618c3fa4a4abfcce20982b44ca01
eiress <104050918+eiress@users.noreply.github.com>
2022-06-18 18:48:46 -0600

    js code

--- Chamber/directory.js
+++ Chamber/directory.js
@@ -1,4 +1,4 @@
-const url=  "https://eiress.github.io/WDD230/Chamber/Json/data.json";
+const requesturl=  "https://eiress.github.io/WDD230/Chamber/Json/data.json";
 
 fetch(url)
   .then(function (response) {
@@ -7,41 +7,41 @@ fetch(url)
   .then(function (jsonObject) {
     console.table(jsonObject); 
     const companies = jsonObject['companies'];
-    companies.forEach(displayProphets);
+    companies.forEach(displayDirectory);
         
     }); 
   
 
 
 
-// function displayProphets(prophet){
-//     let card = document.createElement('section');
-//     let h2 = document.createElement('h2');
-//     let img = document.createElement('img');
-//     let pob = document.createElement('p');
-//     let dob = document.createElement('p');
+function displayDirectory(prophet){
+    let name = document.createElement('p');
+    let address = document.createElement('p');
+    let img = document.createElement('img');
+    let phone = document.createElement('p');
+    let web = document.createElement('p');
 
-//     h2.textContent = prophet.name + ' '+ prophet.lastname;
-//     dob.textContent = `${prophet.birthdate}`;
-//     pob.textContent = `${prophet.birthplace}`;
-//     img.setAttribute('src', prophet.imageurl);
-//     img.setAttribute
-//     card.appendChild(h2);
-//     card.appendChild(dob);
-//     card.appendChild(img);
-//     card.appendChild(pob);
-//     document.querySelector('.cards').appendChild(card);
-// }
+    name.textContent = prophet.name + ' '+ prophet.lastname;
+    phone.textContent = `${prophet.phoneNumber}`;
+    web.textContent = `${prophet.birthplace}`;
+    img.setAttribute('src', prophet.imageurl);
+    img.setAttribute
+    card.appendChild(h2);
+    card.appendChild(dob);
+    card.appendChild(img);
+    card.appendChild(pob);
+    document.querySelector('.cards').appendChild(card);
+}
 
-// function displayTable(prophet){
-//     let list_row = document.createElement('tr');
-//     let td_name = document.createElement('td');
-//     td_name.textContent = prophets.name + ' ' + prophet.lastname;
+function displayTable(prophet){
+    let list_row = document.createElement('tr');
+    let td_name = document.createElement('td');
+    td_name.textContent = prophets.name + ' ' + prophet.lastname;
 
-//     let td_birthdate = document.createElement('td');
-//     td_birthdate.textContent = prophet.birthdate;
+    let td_birthdate = document.createElement('td');
+    td_birthdate.textContent = prophet.birthdate;
 
-//     list_row.appendChild(td_name);
-//     list_row.appendChild(td_birthdate);
-//     document.querySelector('table').appendChild(list_row);
-// }
+    list_row.appendChild(td_name);
+    list_row.appendChild(td_birthdate);
+    document.querySelector('table').appendChild(list_row);
+}
