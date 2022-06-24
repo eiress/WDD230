a03b92458b2e47fe6d358fb099008d633eb00311
eiress <104050918+eiress@users.noreply.github.com>
2022-06-24 12:30:17 -0600

    update json

--- Chamber/directory.js
+++ Chamber/directory.js
@@ -2,51 +2,93 @@ const requesturl=  "https://eiress.github.io/WDD230/Chamber/Json/data.json";
 
 
 
-fetch(requesturl)
+
+
+
+
+
+function displayDirectory(){
+  let section = document.querySelectorAll('section');
+    section.forEach((item) => {
+      item.remove();
+});
+  let row = document.querySelectorAll('tr');
+    row.forEach((item) => {
+      item.remove();
+
+    });
+    
+  fetch(requesturl)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
     const companies = jsonObject['companies'];
-    companies.forEach(displayDirectory);
-    
-    }); 
+    companies.forEach(makeCards);
+    console.table(companies);
+        }); 
 
-// getcompanies(requesturl);
-  
-document.getElementById("myIcon").addEventListener("click", displayDirectory);
-document.getElementById("myList").addEventListener("click", displayTable);
+  console.log('displayDirectory');
+
+function makeCards(companies){
 
 
-function displayDirectory(companies){
-  console.table(companies);
+
+
+    let card = document.createElement('section');
     let name = document.createElement('p');
     let address = document.createElement('p');
     let img = document.createElement('img');
     let phone = document.createElement('p');
-    let web = document.createElement('p');
-    let card = document.querySelector('.card');
+    let web = document.createElement('a');
+    // let card = document.querySelector('.card');
 
 
     name.textContent = companies.name;
     phone.textContent = `${companies.phoneNumber}`;
     web.textContent = `${companies.website}`;
+    web.setAttribute("href",companies.website);
     img.setAttribute('src', `./${companies.image}`);
     console.log(`./${companies.image}`)
     img.setAttribute('alt', companies.name);
-    address.textContent= companies.address;
+    address.textContent= companies.Address;
     phone.textContent = companies.phone;
 
-    
+    card.appendChild(img);
     card.appendChild(name);
     card.appendChild(address);
-    card.appendChild(img);
     card.appendChild(phone);
     card.appendChild(web);
-      //document.querySelector('.card').appendChild(card);
+    document.querySelector('.card').appendChild(card);
+ }
 }
 
-function displayTable(companies){
+function displayTable(){
+  let section = document.querySelectorAll('section');
+    section.forEach((item) => {
+      item.remove();
+});
+let row = document.querySelectorAll('tr');
+    row.forEach((item) => {
+      item.remove();
+
+    });
+  fetch(requesturl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    const companies = jsonObject['companies'];
+    companies.forEach(makeList);
+    console.table(companies);
+        }); 
+
+  console.log('displayTable');
+
+function makeList(companies){
+
+  console.log('displayTable');
+  let section
     let list_row = document.createElement('tr');
 
     let name = document.createElement('td');
@@ -59,17 +101,21 @@ function displayTable(companies){
     name.textContent = companies.name;
     phone.textContent = `${companies.phoneNumber}`;
     web.textContent = `${companies.website}`;
-    img.setAttribute('src', `./${companies.image}`);
-    console.log(`./${companies.image}`)
-    img.setAttribute('alt', companies.name);
+
     address.textContent= companies.address;
     phone.textContent = companies.phone;
 
     list_row.appendChild(name);
     list_row.appendChild(address);
-    list_row.appendChild(img);
+    
     list_row.appendChild(phone);
     list_row.appendChild(web);
     document.querySelector('table').appendChild(list_row);
+    
+ }
 }
 
+document.getElementById("myIcon").addEventListener("click", displayDirectory);
+document.getElementById("myList").addEventListener("click", displayTable);
+
+displayDirectory();
